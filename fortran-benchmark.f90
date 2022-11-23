86bfac491fc093573b9a842d8ca251b30a423412
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2022-11-23 21:15:18 +0100

    Projektdateien hinzufügen.

---
+++ fortran-benchmark.f90
@@ -0,0 +1,25 @@
+!  fortranbenchmark.f90 
+!
+!  FUNCTIONS:
+!  fortranbenchmark - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: fortranbenchmark
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program fortranbenchmark
+
+    implicit none
+
+    ! Variables
+
+    ! Body of fortranbenchmark
+    print *, 'Hello World'
+
+    end program fortranbenchmark
+
